fe78072be58be222e818a5c04d362aace7b454e8
Emanarmila <86047537+Tesaa904@users.noreply.github.com>
2021-10-29 15:30:27 +0700

    Add files via upload

---
+++ js/demo/form-wizard.js
@@ -0,0 +1,185 @@
+
+// Form-Wizard.js
+// ====================================================================
+// This file should not be included in your project.
+// This is just a sample how to initialize plugins or components.
+//
+// - Designbudy.com -
+
+
+$(document).ready(function() {
+
+
+
+	// FORM WIZARD
+	// =================================================================
+	// Require Bootstrap Wizard
+	// http://vadimg.com/twitter-bootstrap-wizard-example/
+	// =================================================================
+
+
+	// CLASSIC FORM WIZARD
+	// =================================================================
+	$('#demo-cls-wz').bootstrapWizard({
+		tabClass		: 'wz-classic',
+		nextSelector	: '.lanjut',
+		previousSelector	: '.kembali',
+		onTabClick: function(tab, navigation, index) {
+			return false;
+		
+		},
+		onTabShow: function(tab, navigation, index) {
+			var $total = navigation.find('li').length;
+			var $current = index+1;
+			var $percent = ($current/$total) * 100;
+			var wdt = 100/$total;
+			var lft = wdt*index;
+			$('#demo-cls-wz').find('.progress-bar').css({width:$percent+'%'});
+
+			// If it's the last tab then hide the last button and show the finish instead
+			if($current >= $total) {
+				$('#demo-cls-wz').find('.lanjut').hide();
+				
+			} else {
+				$('#demo-cls-wz').find('.lanjut').show();
+				
+		}
+	});
+
+
+
+	// FORM WIZARD WITH VALIDATION
+	// =================================================================
+	$('#demo-bv-wz').bootstrapWizard({
+		tabClass		: 'wz-steps',
+		nextSelector	: '.lanjut',
+		previousSelector	: '.kembali',
+		onTabClick: function(tab, navigation, index) {
+			return false;
+		
+		},
+		onTabShow: function(tab, navigation, index) {
+			var $total = navigation.find('li').length;
+			var $current = index+1;
+			var $percent = (index/$total) * 100;
+			var margin = (100/$total)/2;
+			$('#demo-bv-wz').find('.progress-bar').css({width:$percent+'%', 'margin': 0 + 'px ' + margin + '%'});
+
+			navigation.find('li:eq('+index+') a').trigger('focus');
+
+
+			// If it's the last tab then hide the last button and show the finish instead
+			if($current >= $total) {
+				$('#demo-bv-wz').find('.lanjut').hide();
+				
+			} else {
+				$('#demo-bv-wz').find('.lanjut').show();
+				
+			}
+		},
+		onNext: function(){
+			isValid = null;
+			$('#demo-bv-wz-form').bootstrapValidator('validate');
+
+
+			if(isValid === false)return false;
+		}
+	});
+
+
+
+
+	// FORM VALIDATION
+	// =================================================================
+	// Require Bootstrap Validator
+	// http://bootstrapvalidator.com/
+	// =================================================================
+
+	var isValid;
+	$('#demo-bv-wz-form').bootstrapValidator({
+		message: 'This value is not valid',
+		feedbackIcons: {
+		valid: 'fa fa-check-circle fa-lg text-success',
+		invalid: 'fa fa-times-circle fa-lg',
+		validating: 'fa fa-refresh'
+		},
+		fields: {
+		username: {
+			message: 'The username is not valid',
+			validators: {
+				notEmpty: {
+					message: 'The username is required.'
+				}
+			}
+		},
+		email: {
+			validators: {
+				notEmpty: {
+					message: 'The email address is required and can\'t be empty'
+				},
+				emailAddress: {
+					message: 'The input is not a valid email address'
+				}
+			}
+		},
+		firstName: {
+			validators: {
+				notEmpty: {
+					message: 'The first name is required and cannot be empty'
+				},
+				regexp: {
+					regexp: /^[A-Z\s]+$/i,
+					message: 'The first name can only consist of alphabetical characters and spaces'
+				}
+			}
+		},
+		lastName: {
+			validators: {
+				notEmpty: {
+					message: 'The last name is required and cannot be empty'
+				},
+				regexp: {
+					regexp: /^[A-Z\s]+$/i,
+					message: 'The last name can only consist of alphabetical characters and spaces'
+				}
+			}
+		},
+		phoneNumber: {
+			validators: {
+				notEmpty: {
+					message: 'The phone number is required and cannot be empty'
+				},
+				digits: {
+					message: 'The value can contain only digits'
+				}
+			}
+		},
+		address: {
+			validators: {
+				notEmpty: {
+					message: 'The address is required'
+				}
+			}
+		}
+		}
+	}).on('success.field.bv', function(e, data) {
+		// $(e.target)  --> The field element
+		// data.bv      --> The BootstrapValidator instance
+		// data.field   --> The field name
+		// data.element --> The field element
+
+		var $parent = data.element.parents('.form-group');
+
+		// Remove the has-success class
+		$parent.removeClass('has-success');
+
+
+		// Hide the success icon
+		//$parent.find('.form-control-feedback[data-bv-icon-for="' + data.field + '"]').hide();
+	}).on('error.form.bv', function(e) {
+		isValid = false;
+	});
+
+
+
+});
